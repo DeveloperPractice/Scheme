c1c721f2e667937f2802294db29204e4eedb5ff2
sivajeebattina <sivajee.battina@mtuity.com>
2015-12-07 15:32:23 +0530

    Files are back

--- SampleGit/AppDelegate.h
+++ SampleGit/AppDelegate.h
@@ -4,7 +4,11 @@
 //
 //  Created by SivajeeBattina on 12/3/15.
 //  Copyright © 2015 Paradigmcreatives. All rights reserved.
+<<<<<<< HEAD
 // Yes I think it's working 
+=======
+//
+>>>>>>> parent of 738d33d... Reverting to 1st change
 
 #import <UIKit/UIKit.h>
 
